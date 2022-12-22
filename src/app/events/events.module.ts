eea1955f8714d32d58d530978cb0dc91c0d1df32
Chris <chrisskalka@yahoo.com>
2022-12-21 22:16:12 -0600

    update material library

--- src/app/events/events.module.ts
+++ src/app/events/events.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { EventsComponent } from './events.component';
 import {MatCardModule} from '@angular/material/card';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
