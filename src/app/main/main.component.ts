d5d8210050e6c2070eed80864eb3840be9006d01
Chris <chrisskalka@yahoo.com>
2023-01-24 22:58:28 -0600

    updated header

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
   activeTab: string = '';
+  isSticky: boolean = false;
+  
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    this.isSticky = window.pageYOffset >= 200;
+  }
 
   constructor() { }
 
